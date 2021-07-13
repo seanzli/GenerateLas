4731e28d4f37277d7f3bdd30540746da04abf4de
seanzli <76615961+seanzli@users.noreply.github.com>
2021-07-13 22:03:06 +0800

    remove build folder/ .vscode folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/sean/Downloads/work/c/GenerateLas
-# Build directory: C:/Users/sean/Downloads/work/c/GenerateLas/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
