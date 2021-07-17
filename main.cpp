90f07e4504689d577c04f86dc3f2da44721165ad
sean <seanzli@outlook.com>
2021-07-16 21:21:42 -0700

    add glog

--- main.cpp
+++ main.cpp
@@ -10,10 +10,16 @@
 
 #include <GenerateLas.hpp>
 
+#include <glog/logging.h>
 
 #include "StructDef.h"
 
 int main(int argc, char* argv[]) {
+    // google-logging-library initailization
+    google::InitGoogleLogging(argv[0]);
+    google::SetLogDestination(google::GLOG_INFO, "./log/log_");
+
+    LOG(INFO) << "log - test" << std::endl;
 
     Point<float> a(1.1,2.2,3.3);
     Point<double> b(1.1,2.2,3.3);
